349e0667226d88e94f1aa670fde3b630f365cdcf
GFT <gaaftechsolution@gmail.com>
2024-01-02 10:58:22 -0400

    initial commit

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ var app = express();
 var PORT = 3000;
 
 app.set('view engine', 'ejs');
+app.use(
+    cors({
+        origin: ['https://webb-service-app.onrender.com', 'http://localhost:3000/'],
+    })
+)
 
 app.get('/', async (req, res)=>{
     res.render('index')
